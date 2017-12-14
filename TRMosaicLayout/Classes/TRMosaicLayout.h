8b418d376fa8d1343e2fc79420b1458626a2a907
phimage <eric.marchand.n7@gmail.com>
2017-12-14 10:34:52 +0100

    Make framework compatible with carthage Fix demo Podfile iOS version Update to Xcode 9

---
+++ TRMosaicLayout/Classes/TRMosaicLayout.h
@@ -0,0 +1,19 @@
+//
+//  TRMosaicLayout.h
+//  TRMosaicLayout
+//
+//  Created by 4D on 14/12/2017.
+//  Copyright © 2017 CocoaPods. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TRMosaicLayout.
+FOUNDATION_EXPORT double TRMosaicLayoutVersionNumber;
+
+//! Project version string for TRMosaicLayout.
+FOUNDATION_EXPORT const unsigned char TRMosaicLayoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TRMosaicLayout/PublicHeader.h>
+
+
